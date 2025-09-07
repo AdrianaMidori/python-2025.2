37a6af506a7e50dfa3d22b9bbf8d2e4f0435a6d6
AdrianaMidori <adrianalaburu@gmail.com>
2025-09-06 23:20:30 -0300

    Jogo da Velha concluído

--- jogodavelha.py
+++ jogodavelha.py
@@ -5,16 +5,64 @@ jogo = Velha()
 # Desenha o tabuleiro inicial
 jogo.desenharTabuleiro()
 
-# Jogada do Humano
-jogadaLinha = int(input('Digite a posição da linha: '))
-jogadaColuna = int(input('Digite a posição da coluna: '))
-jogo.tabuleiro[jogadaLinha][jogadaColuna] = 'X'
-jogo.desenharTabuleiro()
+while True:
+    # Jogada do Humano
+    entradaLinha = input('Digite a linha (0-2) ou "SAIR": ')
+    if (entradaLinha.upper() == "SAIR"):
+        break
+    entradaColuna = input('Digite a coluna(0-2) ou "SAIR": ')
+    if entradaColuna == "SAIR":
+        break
+    
+    #Se alguma entrada não for número, pedir para fazer nova entrada
+    if (not(entradaLinha.isdigit())) or (not(entradaColuna.isdigit)) or (entradaLinha=='') or (entradaColuna ==''):
+        print('Entrada inválida')
+        continue
+    
+    jogadaLinha = int(entradaLinha)
+    jogadaColuna = int(entradaColuna)
+    if (jogadaLinha > 2) or (jogadaColuna > 2):
+        print('Entrada deve ser entre 0 e 2')
+        continue
+    
+    #Se a posição desejada estiver vazia
+    if jogo.tabuleiro[jogadaLinha][jogadaColuna] == ' ':
+        jogo.receberJogada(jogadaLinha, jogadaColuna, 'X')
+        jogo.desenharTabuleiro()
+    else:
+        print('Jogada inválida')
+        continue
 
-# Jogada da Máquina
-jogadaLinha = jogo.jogarMaquina()
-jogadaColuna = jogo.jogarMaquina()
-jogo.tabuleiro[jogadaLinha][jogadaColuna] = 'O'
-print("Maquina jogou")
-jogo.desenharTabuleiro()
+    #Verificar se houve vencedor
+    temVencedor = jogo.verificarVencedor()
+    if temVencedor:
+        jogo.desenharTabuleiro()
+        jogo.mostrarMsgVencedor()
+        break
 
+    # Jogada da Máquina
+    temEspaco = jogo.verEspacoNoTabuleiro() 
+    if temEspaco:  
+        jogadaLinha = jogo.jogarMaquina()
+        jogadaColuna = jogo.jogarMaquina()  
+
+        #Caso seja sorteado uma posição já preenchida, continuar sorteando nova posição
+        while (jogo.tabuleiro[jogadaLinha][jogadaColuna] != ' '):
+            jogadaLinha = jogo.jogarMaquina()
+            jogadaColuna = jogo.jogarMaquina()
+
+        jogo.receberJogada(jogadaLinha, jogadaColuna, 'O')
+        print("Máquina jogou")
+        jogo.desenharTabuleiro()
+
+    #Verificar se houve vencedor
+    temVencedor = jogo.verificarVencedor()
+    if temVencedor:
+        jogo.desenharTabuleiro()
+        jogo.mostrarMsgVencedor()
+        break
+    elif (not temEspaco):
+        jogo.desenharTabuleiro()
+        print('Não há mais opções de jogada. Jogo EMPATADO!')
+        break
+    
--- velha.py
+++ velha.py
@@ -3,7 +3,7 @@ import random
 class Velha():
     def __init__(self):
         self.tabuleiro = [[' ', ' ', ' '] ,[' ', ' ', ' '] , [' ', ' ', ' ']]
-
+        self.vencedor = ''
 
     def desenharTabuleiro(self):
         print(' ' + self.tabuleiro[0][0] + ' | ' + self.tabuleiro[0][1] + ' | ' + self.tabuleiro[0][2])
@@ -12,21 +12,88 @@ class Velha():
         print('-----------') 
         print(' ' + self.tabuleiro[2][0] + ' | ' + self.tabuleiro[2][1] + ' | ' + self.tabuleiro[2][2])
 
-    def receberJogada(self, linha, coluna):
-        if self.linha == 0:
-            self.tabuleiro[0].insert(coluna, 'X')
-        elif self.linha == 1:
-            self.tabuleiro[1].insert(coluna, 'X')
-        elif self.linha == 2:
-            self.tabuleiro[2].insert(coluna, 'X')
+    def receberJogada(self, linha, coluna, valor):
+        self.linha = linha
+        self.coluna = coluna
+        self.valor = valor
+        self.tabuleiro[linha][coluna] = self.valor           
 
     def jogarMaquina(self):
         return random.randint(0, 2)  
         
 
+    def verEspacoNoTabuleiro(self):
+        temEspaco = False
+        for itemLinha in self.tabuleiro:
+            for itemColuna in itemLinha:
+                if itemColuna == ' ':
+                    temEspaco = True
+        return temEspaco        
+        
+    def verVencedorNasLinhas(self):
+        lista = []
+        for itemLinha in self.tabuleiro:
+            for itemColuna in itemLinha:
+                lista.append(itemColuna)
+
+            #Se os elementos da lista são iguais, houve vencedor
+            if (lista[0] != ' ') and (lista[0] == lista[1]) and (lista[0] == lista[2]):
+                self.vencedor = lista[0]
+                return True
+            lista =[]
+
+        return False
+
+    def verVencedorNasColunas(self):
+        lista =[]
+        for coluna in range(3):
+            for linha in range(3):
+                lista.append(self.tabuleiro[linha][coluna])
+                
+            if (lista[0] != ' ') and (lista[0] == lista[1]) and (lista[0] == lista[2]):
+                self.vencedor = lista[0]
+                return True
+            lista = [] 
+        return False  
+
+    def verVencedorNasDiagonais(self):
+        if (self.tabuleiro[0][0] != ' ') and \
+            (self.tabuleiro[0][0]==self.tabuleiro[1][1]) and \
+            (self.tabuleiro[0][0]== self.tabuleiro[2][2]):
+
+            self.vencedor = self.tabuleiro[0][0]
+            return True
+        
+        if (self.tabuleiro[0][2] != ' ') and \
+            (self.tabuleiro[0][2]==self.tabuleiro[1][1]) and \
+            (self.tabuleiro[0][2]== self.tabuleiro[2][0]):
+            
+            self.vencedor = self.tabuleiro[0][2]
+            return True
+        
+        return False
+
+    def verificarVencedor(self):
+        temVencedorLinha = self.verVencedorNasLinhas()
+        temVencedorColuna = self.verVencedorNasColunas()
+        temVencedorDiagonal = self.verVencedorNasDiagonais()
+        if temVencedorLinha or temVencedorColuna or temVencedorDiagonal:
+            return True
+
+    def mostrarMsgVencedor(self):
+        if self.vencedor == 'X':
+            print('PARABÉNS! VOCÊ VENCEU!')  
+        elif self.vencedor == 'O':
+            print('FIM DE JOGO! A máquina venceu!')   
 
 
 
+
+
+
+   
+
+    
 
 
 
