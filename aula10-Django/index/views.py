37c075e36bd7875d5b1cbe65898f7d4bada3161c
AdrianaMidori <adrianalaburu@gmail.com>
2025-09-30 19:55:20 -0300

    criação de nova página (aplicação) index em sala

--- aula10-Django/index/views.py
+++ aula10-Django/index/views.py
@@ -5,15 +5,3 @@ from django.http import HttpResponse
 def index(request):
     #return HttpResponse('Olá Mundo!')
     return render(request, 'index.html')
-
-def locais_bs(request):
-    return render(request, 'locais_bs.html')
-
-def contato_bs(request):
-    return render(request, 'contato_bs.html')
-
-def imagens_bs(request):
-    return render(request, 'imagens_bs.html')
-
-def sobre_bs(request):
-    return render(request, 'sobre_bs.html')
